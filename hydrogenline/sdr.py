5210f9d56aa14b80fc29485ad8c9625302fa94f4
on5vo <contact@on5vo.be>
2024-12-23 22:30:21 +0100

    Docstrings

--- hydrogenline/sdr.py
+++ hydrogenline/sdr.py
@@ -1,29 +1,31 @@
 import numpy as np
-
 from rtlsdr import RtlSdr
-
 from typing import List, Callable
 from numpy.typing import NDArray
 
 class SDR:
 
     def __init__(self,
-                 sample_rate: int = 2048000000,
+                 sample_rate: int = 2048000,
                  center_freq: int = 1420000000,
                  gain: float = 0.0,
                  bins: int = 2048
                  ) -> None:
-        
-        # Init properties
-        self._sample_rate = None
-        self._center_freq = None
-        self._gain = None
+        """
+        Initialize the SDR wrapper.
+
+        Parameters:
+        ---
+        - sample_rate: Sampling rate in Hz (default 2048 kHz)
+        - center_freq: Center frequency in Hz (default 1.42 GHz)
+        - gain: Gain setting in dB (default 0.0)
+        - bins: Number of frequency bins for FFT (default 2048)
+        """
 
         self.bins = bins
         
         # Setup RTL SDR
         self.dongle = RtlSdr()
-        self.sample_rate = sample_rate
         self.sample_rate = sample_rate
         self.center_freq = center_freq
         self.dongle.set_agc_mode(False)
@@ -31,9 +33,6 @@ class SDR:
         self.dongle.set_direct_sampling(False)
         self.dongle.set_manual_gain_enabled(True)
         self.gain = gain
-
-    def get_valid_gains(self) -> List:
-        return self.dongle.valid_gains_db
     
     @property
     def gain(self) -> float:
@@ -62,17 +61,54 @@ class SDR:
         self.dongle.center_freq = center_freq
         self._center_freq = self.dongle.center_freq
 
-    def get_samples(self) -> NDArray:
+    def get_valid_gains(self) -> List[float]:
+        """
+        Returns the list of valid gain values for the RTL-SDR.
+        """
+        return self.dongle.valid_gains_db
+
+    def get_samples(self) -> NDArray[np.float32]:
+        """
+        Get a sample from the RTL-SDR.
+        
+        Returns:
+        ---
+        - NumPy array of samples.
+        """
         return self.dongle.read_samples(num_samples=self.bins)
     
-    def to_psd(self, x: NDArray, window: Callable) -> NDArray:
-        PSDw = np.sum(np.power(window(self.bins), 2))
-        return np.power(np.abs(np.fft.fftshift(np.fft.fft(x*window(self.bins)))), 2)/self.sample_rate/PSDw
+    def to_psd(self, x: NDArray, window: Callable) -> NDArray[np.float64]:
+        """
+        Convert samples to Power Spectral Density (PSD) using FFT.
+        
+        Parameters:
+        ---
+        - x: Samples to be transformed.
+        - window: Window function for smoothing.
+        
+        Returns:
+        ---
+        - PSD of the samples.
+        """
+        psd = np.power(np.abs(np.fft.fftshift(np.fft.fft(x*window(self.bins)))), 2)
+        return psd / self.sample_rate / np.sum(np.power(window(self.bins), 2))
     
     def get_frequency(self) -> NDArray:
         return self.center_freq + np.linspace(-1,1,num=self.bins)*self.sample_rate/2
     
-    def get_averaged_spectrum(self, averages: int, windows: List[Callable]) -> NDArray:
+    def get_averaged_spectrum(self, averages: int, windows: List[Callable]) -> NDArray[np.float64]:
+        """
+        Get an averaged spectrum from multiple FFT samples.
+
+        Parameters:
+        ---
+        - averages: The number of times to average the spectrum.
+        - windows: List of window functions to apply for smoothing.
+
+        Returns:
+        ---
+        - Averaged power spectral density.
+        """
         num_windows = len(windows)
         S = np.zeros((num_windows, self.bins))
 
